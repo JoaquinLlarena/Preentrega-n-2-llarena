2563258c2306394fe8a86e73dfdc579b2f972e3d
Joaquin Llarena <joa.llarena04@gmail.com>
2023-07-13 21:06:11 -0300

    first-commit

--- main.js
+++ main.js
@@ -3,67 +3,98 @@
 
 
 //tienda de ropa 
+alert("Bienvenido a TacticalShop");
 
- alert("bienvenido a TacticalShop")
+let realizarCompra = true;
+
+while (realizarCompra) {
+  let consulta = confirm("¿Quieres iniciar una compra?");
+
+  if (consulta) {
+    let producto;
+    let seleccionValida = false;
+
+    do {
+      producto = prompt("¿Qué producto necesitas?\n◉ Buzo\n◉ Pantalón\n◉ Remera\n◉ Zapatilla").toLowerCase();
+
+      switch (producto) {
+        case "buzo":
+          alert("Elegiste buzo");
+          seleccionValida = true;
+          break;
+        case "pantalon":
+          alert("Elegiste pantalón");
+          seleccionValida = true;
+          break;
+        case "zapatilla":
+          alert("Elegiste zapatilla");
+          seleccionValida = true;
+          break;
+        case "remera":
+          alert("Elegiste remera");
+          seleccionValida = true;
+          break;
+        default:
+          alert("Elige un producto válido por favor");
+      }
+    } while (!seleccionValida);
+
+    // Talle del producto
+    let talle;
+    let talleValido = false;
+
+    do {
+      talle = prompt("¿Qué talle estás buscando?\n◉ S\n◉ M\n◉ X\n◉ XL").toLowerCase();
+
+      switch (talle) {
+        case "s":
+          alert("Talle S");
+          talleValido = true;
+          break;
+        case "m":
+          alert("Talle M");
+          talleValido = true;
+          break;
+        case "x":
+          alert("Talle X");
+          talleValido = true;
+          break;
+        case "xl":
+          alert("Talle XL");
+          talleValido = true;
+          break;
+        default:
+          alert("Elige un talle válido por favor");
+      }
+    } while (!talleValido);
+
+    let continuar = confirm("¿Deseas realizar otra compra?");
+
+    if (!continuar) {
+      realizarCompra = false;
+    }
+  } else {
+    realizarCompra = false;
+  }
+}
+
+alert("Gracias por su compra. ¡Hasta luego!");
+
+ 
 
 
- let consulta = confirm("¿quieres iniciar una compra?")
-
-  if (consulta == true) {
-
-   let producto = prompt("que producto necesitas: \n ◉buzo \n ◉pantalon \n ◉remera \n ◉zapatilla")
-  
 
 
-//producto a elegir
-   switch (producto) {
-     case "buzo":
-       alert("elegiste buzo");
-       break;
-     case "pantalon":
-       alert("elegiste pantalon");
-       break;
-
-     case "zapatilla":
-       alert("elegiste zapatilla");
-       break;
-     case "remera":
-       alert("elegiste remera");
-       break;
-     default:
-       alert("elegi un producto porfavor")
-   }
 
 
-   //talle del producto
-   let talle = prompt("que talle estas buscando \n ◉S \n ◉M \n ◉X \n ◉XL")
 
-   switch (talle) {
-     case "s":
-       alert("talle S");
-       break;
-     case "m":
-      alert("talle M");
-       break;
-     case "x":
-       alert("talle X");
-       break;
-     case "xl":
-       alert("talle Xl");
-      break;
-       default:
-        alert("elegi un talle porfavor")
 
-   }
 
-alert ("gracias por su compra")
- }
- else {alert("hasta luego")}
 
  //crear usuario 
 function crearUsuario () {
   if (usuario = confirm("¿quieres crear un usuario?")) {
-    let usuario = prompt ("ingresa tu nombre")
+    let usuario = prompt ("ingresa tu nombre").toLowerCase()
     let contraseña = prompt ("ingresa tu contraseña")
     alert ("bienvenido  " + usuario)
   
